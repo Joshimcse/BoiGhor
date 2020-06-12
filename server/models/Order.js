924c9916e7bca8ef8714ebf0eea6c712b96ceb88
Joshimcse <joshimcse@gmail.com>
2020-06-12 23:32:50 +0600

    implement order schema

--- server/models/Order.js
+++ server/models/Order.js
@@ -1,6 +1,66 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const orderSchema = new Schema({});
+const orderSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  },
+  books: [
+    {
+      book: {
+        type: Schema.Types.ObjectId,
+        ref: 'Book',
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        default: 1,
+      },
+    },
+  ],
+  vat: {
+    type: Number,
+    required: true,
+  },
+  totalPrice: {
+    type: Number,
+    required: true,
+  },
+  paymentType: {
+    type: String,
+    required: true,
+  },
+  shippingAdress: {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+    },
+    phone: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+  },
+  isShipped: {
+    type: Boolean,
+    default: false,
+  },
+  isCancelOrder: {
+    type: Boolean,
+    default: false,
+  },
+  Date: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
 module.exports = mongoose.model('Order', orderSchema);
